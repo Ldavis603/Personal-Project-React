260fed9bf17931a36d3a50041d7c872767b7e4a3
LaJanae Davis <lajanaedavis@gmail.com>
2024-06-20 21:46:58 -0400

    Components updated

--- src/components/Create.js
+++ src/components/Create.js
@@ -1,15 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Create = () => {
+  const [newExercise, setNewExercise] = useState({
+    exerciseName: "",
+    exerciseType: "",
+    description: "",
+  });
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+
+    setNewExercise({
+      ...newExercise,
+      [name]: value,
+    });
+  };
+  const handleCreateFormSubmit = (e) => {
+    e.preventDefault();
+    fetch("http://localhost:4000/api/exercise/create/new", {
+      method: "POST",
+      body: JSON.stringify(newExercise),
+    })
+      .then((response) => response.json())
+      .then((result) => {
+        console.log(result.data);
+        setNewExercise(result.data);
+      });
+  };
   return (
     <div>
-      <form>
+      <form onSubmit={handleCreateFormSubmit}>
         <label htmlFor="exerciseName">Exercise</label>
         <input
           type="text"
           id="exerciseName"
           name="exerciseName"
           placeholder="Exercise"
+          value={newExercise.exerciseName}
+          onChange={handleInputChange}
         />
 
         <label htmlFor="exerciseType">Type</label>
@@ -18,6 +45,8 @@ const Create = () => {
           id="exerciseType"
           name="exerciseType"
           placeholder="Type"
+          value={newExercise.exerciseType}
+          onChange={handleInputChange}
         />
 
         <label htmlFor="description">Description</label>
@@ -26,7 +55,11 @@ const Create = () => {
           id="description"
           name="description"
           placeholder="Description"
+          value={newExercise.exerciseType}
+          onChange={handleInputChange}
         />
+
+        <button>Create Exercise</button>
       </form>
     </div>
   );
--- src/components/Update.js
+++ src/components/Update.js
@@ -1,15 +1,51 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import {useParams} from "react-router-dom";
 
 const Update = () => {
+  const {id} = useParams()
+  const [updatedExercise, setUpdatedExercise] = useState({
+    exerciseName: "",
+    exerciseType: "",
+    description: "",
+  });
+  useEffect(() => {
+    fetch(`http://localhost:4000/api/exercise/${id}`)
+      .then((response) => response.json())
+      .then((result) => {
+        console.log(result.data);
+        setUpdatedExercise(result.data);
+      });
+  });
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setUpdatedExercise({
+      ...updatedExercise,
+      [name]: value,
+    });
+  };
+  const handleUpdateFormSubmit = (e) => {
+    e.preventDefault();
+    fetch(`http://localhost:4000/api/exercise/edit/${id}`, {
+      method: "POST",
+      body: JSON.stringify(updatedExercise),
+    })
+      .then((response) => response.json())
+      .then((result) => {
+        console.log(result.data);
+        setUpdatedExercise(result.data);
+      });
+  };
   return (
     <div>
-      <form>
+      <form on onSubmit={handleUpdateFormSubmit}>
         <label htmlFor="exerciseName">Exercise</label>
         <input
           type="text"
           id="exerciseName"
           name="exerciseName"
           placeholder="Exercise"
+          value={updatedExercise.exerciseName}
+          onChange={handleInputChange}
         />
 
         <label htmlFor="exerciseType">Type</label>
@@ -18,6 +54,8 @@ const Update = () => {
           id="exerciseType"
           name="exerciseType"
           placeholder="Type"
+          value={updatedExercise.exerciseType}
+          onChange={handleInputChange}
         />
 
         <label htmlFor="description">Description</label>
@@ -26,6 +64,8 @@ const Update = () => {
           id="description"
           name="description"
           placeholder="Description"
+          value={updatedExercise.description}
+          onChange={handleInputChange}
         />
       </form>
     </div>
